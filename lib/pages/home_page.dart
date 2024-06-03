10ae244af0e604e66f253e0218bfdd37f41fff55
Lattaruolo Andrea <lattaruolo.96@gmail.com>
2024-06-03 14:26:40 +0200

    Pages_creation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:gemini_app/main.dart';
-import 'package:gemini_app/pages/login.dart';
+import 'package:gemini_app/components/my_drawer.dart';
+import 'package:gemini_app/pages/setting_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,13 +10,31 @@ class HomePage extends StatelessWidget {
     FirebaseAuth.instance.signOut();
   }
 
+  void openMenu() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( backgroundColor: Colors.pink[300],actions: [
-        IconButton(onPressed: signOutUser, icon: Icon(Icons.logout))
-      ]),
-      body: Center(child: Text('Welcome, you are logged in')),
-    );
+        appBar: AppBar(
+            title: Text(
+              "Kamilia",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22),
+            ),
+            backgroundColor: Colors.pink[300]),
+        drawer: MyDrawer(),
+        body: SafeArea(
+            child: Column(
+          children: [
+            SizedBox(height:  40),
+            Center(child: Text('Welcome, you are logged in')),
+            SizedBox(height:  40),
+            Center(child: Text('Welcome, you are logged in')),
+            SizedBox(height:  40),
+            Center(child: Text('Welcome, you are logged in'))
+          ],
+        )));
   }
 }
