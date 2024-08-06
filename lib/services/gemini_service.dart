0260a5ba292f3c6a5de74c9f73c25f27960377a6
Lattaruolo Andrea <lattaruolo.96@gmail.com>
2024-08-06 19:38:44 +0200

    geminy ai

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -1,99 +1,131 @@
 import 'package:gemini_app/components/message_class.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'dart:io';
+const apiKey = "AIzaSyDGyDL2iX5lmySpjU4WUMwR2gRCRiFIMBA";
 
 class GeminyService {
+  final model = GenerativeModel(
+    model: 'gemini-1.5-flash', // here goes the ID of the tuned model
+    apiKey: apiKey,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    generationConfig: GenerationConfig(
+      temperature: 1,
+      topK: 64,
+      topP: 0.95,
+      maxOutputTokens: 8192,
+      responseMimeType: 'text/plain',
+    ),
+  );
 
-  Future<dynamic> processUserInput(dynamic input){
+  callToGeminy(input) async{
+      final content = [Content.text(input)];
+      final response = await model.generateContent([input]);
+  // https://www.youtube.com/watch?v=VwpDvvNjN2I
+      return response.text;
+  }
+
+
+  Future<dynamic> processUserInput(dynamic input) {
     //TODO this is the call to geminy AI
-    // "sender":false add the "sender" : false, to the map
-    return input;
+    return callToGeminy(input);
   }
 
   Future<EmotionCategorizeClass> categolizeListOfEmotions(
       List<String> emotions) async {
-    await Future.delayed(const Duration(seconds: 2));
-    var ll = {
-      "input": [
-        "joy",
-        "anger",
-        "fear",
-        "curiosity",
-        "shame",
-        "gratitude",
-        "boredom"
-      ],
-      "type": "emotion-categorize",
-      "output": [
-        {
-          "emotion": "joy",
-          "evaluation": "10",
-          "description": "A feeling of happiness, contentment, and pleasure.",
-          "category": "Basic Emotion"
-        },
-        {
-          "emotion": "anger",
-          "evaluation": "2",
-          "description":
-              "A feeling of intense displeasure, hostility, and aggression.",
-          "category": "Basic Emotion"
-        },
-        {
-          "emotion": "fear",
-          "evaluation": "2",
-          "description": "A feeling of apprehension, worry, and anxiety.",
-          "category": "Basic Emotion"
-        },
-        {
-          "emotion": "curiosity",
-          "evaluation": "7",
-          "description":
-              "A feeling of intrigue, interest, and inquisitiveness.",
-          "category": "Cognitive Emotion"
-        },
-        {
-          "emotion": "shame",
-          "evaluation": "2",
-          "description":
-              "A feeling of embarrassment, humiliation, and remorse.",
-          "category": "Social Emotion"
-        },
-        {
-          "emotion": "gratitude",
-          "evaluation": "9",
-          "description":
-              "A feeling of thankfulness, appreciation, and indebtedness.",
-          "category": "Social Emotion"
-        },
-        {
-          "emotion": "boredom",
-          "evaluation": "3",
-          "description":
-              "A feeling of uninterest, restlessness, and lack of stimulation.",
-          "category": "Social Emotion"
-        }
-      ]
-    };
-    EmotionCategorizeClass b = EmotionCategorizeClass(ll);
+        String input = ' Given a list of emotions provide a number between 1 to 10 where 1 means a negative emotion 5 in a neutral emotion an 10 is a positive emotion: ###input:${emotions.toList()}### ###output: [{"emotion":"emotion1","evaluation":"value for emotion1","decription":"emotion description","category":"Emotion1 category" },{"emotion":"emotion3","evaluation":"value for emotion3","decription":"emotion description","category":"Emotion3 category"},{"emotion":"emotion2","evaluation":"value for emotion2","decription":"emotion description","category":"Emotion2 category"}] ###';
+    
+        dynamic res = callToGeminy(input);
+    // await Future.delayed(const Duration(seconds: 2));
+    // var ll = {
+    //   "input": [
+    //     "joy",
+    //     "anger",
+    //     "fear",
+    //     "curiosity",
+    //     "shame",
+    //     "gratitude",
+    //     "boredom"
+    //   ],
+    //   "type": "emotion-categorize",
+    //   "output": [
+    //     {
+    //       "emotion": "joy",
+    //       "evaluation": "10",
+    //       "description": "A feeling of happiness, contentment, and pleasure.",
+    //       "category": "Basic Emotion"
+    //     },
+    //     {
+    //       "emotion": "anger",
+    //       "evaluation": "2",
+    //       "description":
+    //           "A feeling of intense displeasure, hostility, and aggression.",
+    //       "category": "Basic Emotion"
+    //     },
+    //     {
+    //       "emotion": "fear",
+    //       "evaluation": "2",
+    //       "description": "A feeling of apprehension, worry, and anxiety.",
+    //       "category": "Basic Emotion"
+    //     },
+    //     {
+    //       "emotion": "curiosity",
+    //       "evaluation": "7",
+    //       "description":
+    //           "A feeling of intrigue, interest, and inquisitiveness.",
+    //       "category": "Cognitive Emotion"
+    //     },
+    //     {
+    //       "emotion": "shame",
+    //       "evaluation": "2",
+    //       "description":
+    //           "A feeling of embarrassment, humiliation, and remorse.",
+    //       "category": "Social Emotion"
+    //     },
+    //     {
+    //       "emotion": "gratitude",
+    //       "evaluation": "9",
+    //       "description":
+    //           "A feeling of thankfulness, appreciation, and indebtedness.",
+    //       "category": "Social Emotion"
+    //     },
+    //     {
+    //       "emotion": "boredom",
+    //       "evaluation": "3",
+    //       "description":
+    //           "A feeling of uninterest, restlessness, and lack of stimulation.",
+    //       "category": "Social Emotion"
+    //     }
+    //   ]
+    // };
+    EmotionCategorizeClass b = EmotionCategorizeClass(res);
     return b;
   }
 
   Future<GroupClass> groupActivities(List<String> activities) async {
-    await Future.delayed(const Duration(seconds: 2));
-    var ll = {
-      // "input": [
-      //   "Go to a museum",
-      //   "Learn about history",
-      //   "Admire the art",
-      //   "Take photos"
-      // ],
-      // "type": "group-activities",
-      "Physical Activities": [],
-      "Entertainment": ["Go to a museum", "Admire the art"],
-      "Learning & Development": ["Learn about history"],
-      "Work & Chores": [],
-      "Social & Personal": ["Take photos"]
-    };
-    GroupClass b = GroupClass(ll);
+    String input ='group together this list of activities  ###${activities.toString()}### across these categories: ["Physical Activities","Entertainment","Learning & Development", "Work & Chores", Social & Personal]. The format of the grouping is {"input":["activity1","activity2","activity3",....],"type":"group-activities","Physical Activities":["activity1"],"Entertainment": [], "Learning & Development":["activity2","activity3"],"Work & Chores":[],"Social & Personal":["activity4","activity5"]}';
+    dynamic res = callToGeminy(input);
+    // await Future.delayed(const Duration(seconds: 2));
+    // var ll = {
+    //   // "input": [
+    //   //   "Go to a museum",
+    //   //   "Learn about history",
+    //   //   "Admire the art",
+    //   //   "Take photos"
+    //   // ],
+    //   // "type": "group-activities",
+    //   "Physical Activities": [],
+    //   "Entertainment": ["Go to a museum", "Admire the art"],
+    //   "Learning & Development": ["Learn about history"],
+    //   "Work & Chores": [],
+    //   "Social & Personal": ["Take photos"]
+    // };
+    GroupClass b = GroupClass(res);
     return b;
   }
 
+
+
+
 }
+
